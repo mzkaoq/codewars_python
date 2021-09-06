db712059094234584b5ba01fea7a219d31d8447f
mzkaoq <mzkaoq333@gmail.com>
2021-09-06 23:06:52 +0200

    done

--- 7kyu Integer Difference/main.py
+++ 7kyu Integer Difference/main.py
@@ -1,8 +1,11 @@
 def int_diff(lst, n):
     returned_value = 0
-    print(lst,n)
+    print(lst, n)
     for i in range(len(lst)):
-        for j in range(i+1, len(lst)):
+        for j in range(i + 1, len(lst)):
             if lst[i] + n == lst[j] or lst[i] - n == lst[j]:
                 returned_value += 1
-    return returned_value
+    if n < 0:
+        return 0
+    else:
+        return returned_value
