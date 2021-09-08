ae37228b20cea27a2d88e197b45edd10034450b5
mzkaoq <mzkaoq333@gmail.com>
2021-09-09 00:05:38 +0200

    done

--- 5kyu What's a Perfect Power anyway/main2.py
+++ 5kyu What's a Perfect Power anyway/main2.py
@@ -1,10 +1,6 @@
 def isPP(n):
-    for i in range(2,round(n**0.5)+1):
-        k = n**(1/i)
+    for i in range(2, round(n ** 0.5) + 1):
+        k = round(n ** (1 / i), 8)
         if k % 1 == 0:
-            return [int(k),i]
+            return [int(k), i]
     return None
-
-
-for i in [4, 8, 9, 16, 25, 27, 32, 36, 49, 64, 81, 100, 121, 125, 128, 144, 169, 196, 216, 225, 243, 256, 289, 324, 343, 361, 400, 441, 484]:
-    print(isPP(i))
